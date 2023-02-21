7f82bc2c86848602bcacb6cfa0f1cf843cc1a73f
kiboma <omayioben@gmail.com>
2023-02-21 15:42:07 +0300

    Add block and result component

---
+++ src/components/Block.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h1>Modal heading</h1>
+
+        </div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.modal{
+    width: 400px;
+    background: greenyellow;
+    margin:100px auto;
+    padding: 20px;
+    border-radius: 10px;    
+
+}
+
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.486);
+    width: 100%;
+    height: 100%;
+    margin-top: -2rem;
+
+
+}
+
+</style>
